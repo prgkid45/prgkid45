fef65c3afceeb9f460c48a78d026bfaf45834350
prgkid45 <137392701+prgkid45@users.noreply.github.com>
2023-06-22 20:00:30 +0300

    Add files via upload

---
+++ turtle_mainloop.py
@@ -0,0 +1,5 @@
+import turtle#turtle modülünü dosyamızda içeri aktardık
+
+kaplumbaga=turtle.Turtle(shape="turtle")#kaplumbaga isminde bir nesne oluşturduk
+kaplumbaga.circle(100)#ekrana daire çizdirdik
+turtle.getscreen()._root.mainloop()#çemberin ekranda kalmasını sağladık
---
+++ turtle_örnek.py
@@ -0,0 +1,15 @@
+import turtle
+wn=turtle.Screen()
+
+t=turtle.Turtle()
+
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+
+
+wn.mainloop()
